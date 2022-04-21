09caf11abfd1ec74b47836b2962c4cdae71bbb5b
hduong18 <85292069+hduong18@users.noreply.github.com>
2022-04-21 23:14:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'em iu à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Con đĩ của anh à',
+    introDesc: `anh chỉ mún lói với iem 1 đìu rằng,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Cố lên con đĩ của anh nhá',
+    desc: 'Anh sẽ mai ở cạnh em con đĩ của anh ạ .',
+    btnYes: 'em sẽ cố gắng ',
+    btnNo: 'không anh cúc đi',
+    question:'hãy mãi cố gắng vì có anh bên cạnh',
+    btnReply: 'trả lời thật lòngggg ',
+    reply: 'hihi em sẽ cố gắng',
+    mess: 'Anh biết mà.Yêu em nhiều nhiều 😘😘.Gòi nào rnhr thì anh đưa bé đi chơi nhá',
+    messDesc: 'mãi iu.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.facebook.com/duong.nguyenhoang.1257
+    messLink: https://www.facebook.com/messages/t/100045550239735
 }
